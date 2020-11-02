c51c20fb0cc5ee2c609485d0da042e22b0b5f76e
Erin <erincconrad@gmail.com>
2020-11-02 12:48:47 -0500

    exception handling for rederive_original_chs

--- prep_data/rederive_original_chs.m
+++ prep_data/rederive_original_chs.m
@@ -2,7 +2,12 @@ function out = rederive_original_chs(chIndices,out,chLabels,oldLabels)
     ch_new = out(:,2);
     
     original_channels = chIndices(ch_new);
-    out = [out(:,1),original_channels'];
+    
+    if size(original_channels,2) == 1
+        out = [out(:,1),original_channels];
+    else
+        out = [out(:,1),original_channels'];
+    end
     
     % Double check
     newLabels = chLabels(ch_new);
