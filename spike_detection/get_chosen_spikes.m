5894796ae88754ef76669eb033c963305952873d
Erin <erincconrad@gmail.com>
2020-11-17 12:21:05 -0500

    added sz times to exclude from spike detection

--- spike_detection/get_chosen_spikes.m
+++ spike_detection/get_chosen_spikes.m
@@ -5,7 +5,7 @@ function get_chosen_spikes(overwrite)
 %4 6 7 8
 
 %% General parameters
-whichPts = [4 6 7 8];
+whichPts = [1 3 5 6 8 9 10 11]; % I believe these are the pts with all available data
 add_clean_times = 0;
 batch_time = 60;
 pt_file = 'pt_w_elecs.mat';
