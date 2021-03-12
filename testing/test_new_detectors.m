0e86b2f7479087fe73f27139fd78a1b52c57e9b0
Erin <erincconrad@gmail.com>
2021-03-12 11:54:05 -0500

    code to test spike detectors

--- testing/test_new_detectors.m
+++ testing/test_new_detectors.m
@@ -14,7 +14,7 @@ rm_bad = 0;
 %% Locations
 locations = implant_files;
 pwname = locations.pwfile;
-scripts_folder = [locations.main_folder,'scripts/'];
+scripts_folder = [locations.script_folder];
 results_folder = [locations.main_folder,'results/'];
 addpath(genpath(scripts_folder));
 out_folder = [results_folder,'testing/',detector,'/'];
